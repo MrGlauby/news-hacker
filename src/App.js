63f087c980e5d082188afb206e11170e26c423d4
Thomas <test@gmail.com>
2023-04-19 17:31:46 +0200

    newsarticle.js in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NewsArticle from "./components/NewsArticle";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="ueber1">Hacker News</h1>
+        <h5>NewsArticle</h5>
 
+        {/* title, url, points, author, time, comments */}
+
+        <NewsArticle //sinds property oder propbs
+        
+          title="willx" url="2443" points="google" author="willy" time="24234" comments="deine mudda"
+
+        />
       </header>
+
+
+
     </div>
   );
 }
