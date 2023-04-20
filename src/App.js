08c51d66ab683780edecea4a275d08b7803dfe5d
Friis Kraas <friis.kraas@gmx.de>
2023-04-20 18:13:00 +0200

    created header and added styling

--- src/App.js
+++ src/App.js
@@ -1,61 +1,69 @@
-import {useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import NewsArticle from "./components/NewsArticle.js";
 
 function App() {
   const [articles, setArticles] = useState([]);
 
-  const userInputElement = document.getElementById("userInput");
-  const userTagElement = document.getElementById("tagSelector");
-
-  function handleSearch(){
-    console.log(userInputElement);
+  function handleSearch() {
+    const userInputElement = document.getElementById("userInput");
+    const userTagElement = document.getElementById("tagSelector");
     let userInput = userInputElement.value;
     let userTag = userTagElement.options[userTagElement.selectedIndex].value;
+
     let targeturl = "http://hn.algolia.com/api/v1/search?query=";
     targeturl += userInput;
-    if(userTag) targeturl += `&tags=${userTag}`;
+    if (userTag) targeturl += `&tags=${userTag}`;
 
     fetch(targeturl)
-    .then((response) => {
-      if (!response.ok)
-        throw new Error(`So, this happened: ${response.status}`);
-      return response.json();
-    })
-    .then((data) => {
-      setArticles(data.hits);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`So, this happened: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.hits);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" id="userInput"/>
-        <select id="tagSelector">
-          <option value="">All</option>
-          <option value="story">Stories</option>
-          <option value="comment">Comments</option>
-          <option value="poll">Polls</option>
-        </select>
-        <button onClick={handleSearch}>Search</button>
+        <div className="branding">
+          <span className="logo">Y</span>
+          <span className="logoName">Hacker News</span>
+        </div>
+        <div>
+          <input type="text" id="userInput" />
+          <select id="tagSelector">
+            <option value="">All</option>
+            <option value="story">Stories</option>
+            <option value="comment">Comments</option>
+            <option value="poll">Polls</option>
+          </select>
+          <button onClick={handleSearch}>Search</button>
+        </div>
       </header>
       <main>
         <ol>
-        {articles.length ?
-          articles.map((article) => 
-            <NewsArticle
-              title={article.story_title}
-              url={article.story_url}
-              points={article.points}
-              author={article.author}
-              time={article.created_at_i}
-              comments={article.num_comments}
-            />
-          ) : <li>Bitte Suche starten</li>}
-          </ol>
+          {articles.length ? (
+            articles.map((article) => (
+              <NewsArticle
+                title={article.story_title || article.title}
+                url={article.story_url || article.url}
+                points={article.points}
+                author={article.author}
+                time={article.created_at_i}
+                comments={article.num_comments}
+              />
+            ))
+          ) : (
+            <li>Bitte Suche starten</li>
+          )}
+        </ol>
       </main>
     </div>
   );
